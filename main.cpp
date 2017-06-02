08d80a421a991836bb46f34fb8689004d21dc31b
Pavitter Ghotra <pavitterghotra@yahoo.de>
2017-06-02 11:34:27 +0200

    First Triangle, First Excercise

--- main.cpp
+++ main.cpp
@@ -51,36 +51,30 @@ int main(int argc, char const *argv[])
 
     GLfloat vertices[] =
     {
-      0.5f,0.5f,-0.0f,
-      0.5f,-0.5f,-0.0f,
-      -0.5f,-0.5f,-0.0f,
-      -0.5f,0.5f,-0.0f
+      -1.0f,-0.5f,-0.0f,
+       0.0f,-0.5f,-0.0f,
+      -0.5f, 0.5f,-0.0f,
+
+      -0.0f,-0.5f,-0.0f,
+       1.0f,-0.5f,-0.0f,
+       0.5f, 0.5f,-0.0f
     };
 
-    GLuint indices[] =
-    { 0,1,3,
-      1,2,3
-    };
 
     GLuint VAO;
     GLuint VBO;
-    GLuint EBO;
     glGenVertexArrays(1,&VAO);
     glGenBuffers(1,&VBO);
-    glGenBuffers(1,&EBO);
 
     glBindVertexArray(VAO);
-    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
 
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3 * sizeof(GL_FLOAT),(GLvoid* )0);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
+
     GLuint vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
@@ -121,11 +115,14 @@ int main(int argc, char const *argv[])
 
       glUseProgram(shaderProgram);
       glBindVertexArray(VAO);
-      glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+      glDrawArrays(GL_TRIANGLES,0,6);
       glBindVertexArray(0);
       glfwSwapBuffers(window);
     }
 
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glDeleteProgram(shaderProgram);
     glfwTerminate();
